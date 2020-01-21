dde2ec9f2362f09af7880841067f47dd6a23555d
YuriiU <Uvarov.yura@gmail.com>
2020-01-21 14:54:05 +0200

    changes

--- lib/colorful_screen.dart
+++ lib/colorful_screen.dart
@@ -8,26 +8,36 @@ class ColorfulScreen extends StatefulWidget {
 
 class _ColorfulScreenState extends State<ColorfulScreen> {
   Color backgroundColor;
+  Color textColor;
 
   @override
   void initState() {
     super.initState();
     backgroundColor = getRandomColor();
+    textColor = getRandomColor();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: GestureDetector(
-            child: Container(
-              decoration: BoxDecoration(color: backgroundColor),
-              child: Center(
-                child: Text('Hey there'),
-              ),
+    return GestureDetector(
+        child: Container(
+          decoration: BoxDecoration(color: backgroundColor),
+          child: Center(
+            child: Text(
+              'Hey there',
+              style:
+                  TextStyle(color: textColor, decoration: TextDecoration.none),
             ),
-            onTap: () => setState(() {
-                  backgroundColor = getRandomColor();
-                })));
+          ),
+        ),
+        onTap: () => changeColors());
+  }
+
+  void changeColors() {
+    setState(() {
+      backgroundColor = getRandomColor();
+      textColor = getRandomColor();
+    });
   }
 
   Color getRandomColor() {
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uvarov_demo/screens/home_screen.dart';
+import 'package:uvarov_demo/colorful_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Demo App',
-      home: HomeScreen(),
+      home: ColorfulScreen(),
     );
   }
 }
